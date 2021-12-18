6e10510179704e0499fbe2692d3f4e75d150683b
CIBI-A <ee18b044@smail.iitm.ac.in>
2021-12-18 20:05:51 +0530

    Changes done

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -1,6 +1,21 @@
 pragma solidity ^0.5.0;
 
-contract Todolist{ 
+contract TodoList{ 
     uint public taskcount=0;
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed; 
+    } 
+    mapping(uint => Task) public tasks;
+
+    constructor() public{
+        createTask("Testing");
+    }
+
+    function createTask(string memory _content) public{
+        taskcount ++;
+        tasks[taskcount]=Task(taskcount,_content,false);
+    }
 }
